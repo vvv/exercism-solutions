19d5e27953653b6a94a5929774cdc6ce838313ab
Valery V. Vorotyntsev <valery.vv@gmail.com>
2016-03-08 13:42:42 +0200

    anagram: Rewrite using functional style

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,14 +1,10 @@
 pub fn anagrams_for<'a>(word: &str, inputs: &[&'a str]) -> Vec<&'a str> {
     let word = word.to_lowercase();
     let word_sorted = sorted(&word);
-    let mut res = vec![];
-    for &w in inputs.iter() {
+    inputs.into_iter().filter_map(|&w| {
         let wl = w.to_lowercase();
-        if wl != word && sorted(&wl) == word_sorted {
-            res.push(w);
-        }
-    }
-    res
+        if wl != word && sorted(&wl) == word_sorted { Some(w) } else { None }
+    }).collect()
 }
 
 fn sorted(word: &String) -> String {
