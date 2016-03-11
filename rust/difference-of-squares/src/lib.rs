7a56c986040cc48f60d7772dae9159a25ed8fc1c
Valery V. Vorotyntsev <valery.vv@gmail.com>
2016-03-11 23:49:12 +0200

    difference-of-squares: Use formulas
    Of course this problem is solvable with formulas! (It's been ages
    since I did any math.)

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,12 +1,13 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let sum = (1..n+1).fold(0, |sum, x| sum + x);
+    let sum = n * (n + 1) / 2;
     sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..n+1).fold(0, |sum, x| sum + x*x)
+    // n^3/3 + n^2/2 + n/6
+    n * (n + 1) * (2*n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
-    square_of_sum(n) - sum_of_squares(n)
+    n * (n*n - 1) * (3*n + 2) / 12
 }
