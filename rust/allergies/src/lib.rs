7d55ce90b52a14bacb94e4103b955aa76551a9e9
Valery V. Vorotyntsev <valery.vv@gmail.com>
2016-03-20 23:21:32 +0200

    allergies: Reimplemented using C-like enums
    Reference: http://rustbyexample.com/custom_types/enum/c_like.html

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,34 +1,20 @@
-#[derive(PartialEq, Debug)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub enum Allergen {
-    Eggs,         //   1 = 2^0
-    Peanuts,      //   2 = 2^1
-    Shellfish,    //   4 = 2^2
-    Strawberries, //   8 = 2^3
-    Tomatoes,     //  16 = 2^4
-    Chocolate,    //  32 = 2^5
-    Pollen,       //  64 = 2^6
-    Cats,         // 128 = 2^7
-}
-
-fn score(a: &Allergen) -> u32 {
-    let p2 = match a {
-        &Allergen::Eggs => 0,
-        &Allergen::Peanuts => 1,
-        &Allergen::Shellfish => 2,
-        &Allergen::Strawberries => 3,
-        &Allergen::Tomatoes => 4,
-        &Allergen::Chocolate => 5,
-        &Allergen::Pollen => 6,
-        &Allergen::Cats => 7,
-    };
-    1 << p2
+    Eggs         = 1 << 0,
+    Peanuts      = 1 << 1,
+    Shellfish    = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes     = 1 << 4,
+    Chocolate    = 1 << 5,
+    Pollen       = 1 << 6,
+    Cats         = 1 << 7,
 }
 
 pub struct Allergies(pub u32);
 
 impl Allergies {
     pub fn is_allergic_to(&self, a: &Allergen) -> bool {
-        self.0 & score(a) != 0
+        self.0 & (*a as u32) != 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
