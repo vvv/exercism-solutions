85e47d6b54898b19b0f655368e6bb06086256c83
Valery V. Vorotyntsev <valery.vv@gmail.com>
2016-04-03 21:45:12 +0300

    word-count: Rewrite in functional style

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
-    let mut res = HashMap::new();
-    for w in s.replace(|c: char| !c.is_alphanumeric(), " ").split_whitespace() {
-        let count = res.entry(w.to_lowercase().to_string()).or_insert(0);
-        *count += 1;
-    }
-    res
+    s.replace(|c: char| !c.is_alphanumeric(), " ").split_whitespace()
+        .fold(HashMap::new(), |mut res, w| {
+            *res.entry(w.to_lowercase().to_string()).or_insert(0) += 1;
+            res
+        })
 }
