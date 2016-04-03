1371a98955109b129d4701511f480d0a87e49be8
Valery V. Vorotyntsev <valery.vv@gmail.com>
2016-04-03 22:06:24 +0300

    word-count: Code without fold() is simpler
    Maybe not... Well, whatever.

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
-    s.replace(|c: char| !c.is_alphanumeric(), " ").split_whitespace()
-        .fold(HashMap::new(), |mut res, w| {
-            *res.entry(w.to_lowercase().to_string()).or_insert(0) += 1;
-            res
-        })
+    let mut res = HashMap::new();
+    for w in s.replace(|c: char| !c.is_alphanumeric(), " ").split_whitespace() {
+        *res.entry(w.to_lowercase().to_string()).or_insert(0) += 1;
+    }
+    res
 }
