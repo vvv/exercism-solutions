87c7738e699895355cfa3a78259e651c615292ca
Valery V. Vorotyntsev <valery.vv@gmail.com>
2016-10-30 02:17:11 +0300

    sieve: the solution by @rwz is even better
    http://exercism.io/submissions/528e873fbcfa489da91e5c565288d240

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -2,11 +2,11 @@ pub struct Sieve;
 
 impl Sieve {
     pub fn primes_up_to(n: i32) -> Vec<i32> {
-        (2..n+1).fold(vec![], |mut acc, i| {
-            match acc.iter().all(|&x| i % x != 0) {
-                true => {acc.push(i); acc},
-                false => acc
-            }
+        (2..n+1).fold(vec![], |mut primes, x| {
+            if primes.iter().all(|&p| x % p != 0) {
+                primes.push(x);
+            };
+            primes
         })
     }
 }
