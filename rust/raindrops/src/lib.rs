10d93f7a32700bf416c3e264c5021ef5965d6851
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-05-12 02:08:45 +0300

    raindrops: Use String's `+` operator

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn raindrops(x: i32) -> String {
     let mut s = String::new();
-    if x % 3 == 0 { s.push_str("Pling"); }
-    if x % 5 == 0 { s.push_str("Plang"); }
-    if x % 7 == 0 { s.push_str("Plong"); }
+    if x % 3 == 0 { s += "Pling"; }
+    if x % 5 == 0 { s += "Plang"; }
+    if x % 7 == 0 { s += "Plong"; }
     if s.is_empty() {
         x.to_string()
     } else {
