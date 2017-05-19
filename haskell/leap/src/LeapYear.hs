10a5520d106439f657f538feb77522c17f0b7ec1
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-05-19 12:24:37 +0100

    haskell/leap: Solved

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,6 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year = div_by 4 && not (div_by 100) || div_by 400
+  where
+    div_by n = year `rem` n == 0
