eb7421a73739a254a07a589101aa05cc968e95f1
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-06-24 18:36:57 +0300

    haskell/bob: Solved

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,13 @@
+{-# OPTIONS_GHC -Werror -Wall #-}
+
 module Bob (responseFor) where
 
+import Data.List (isSuffixOf, dropWhileEnd)
+import Data.Char (isSpace, isAlpha, toUpper)
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | all isSpace xs = "Fine. Be that way!"
+  | any isAlpha xs && all (\c -> toUpper c == c) xs = "Whoa, chill out!"
+  | "?" `isSuffixOf` dropWhileEnd isSpace xs = "Sure."
+  | otherwise = "Whatever."
