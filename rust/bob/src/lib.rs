db25fa8d41be081ae3c9d7e3bd3fab41fa9f76ad
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-06-27 11:40:44 +0300

    rust/bob: Update solution to pass a stricter set of tests
    The "stricter set of tests" has not been merged into the Rust track yet;
    the PR is here: https://github.com/exercism/rust/pull/311

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,14 +1,13 @@
 pub fn reply(phrase: &str) -> &str {
     if phrase.trim().is_empty() {
-        "Fine. Be that way!"
+        return "Fine. Be that way!";
+    }
+    let s = phrase.trim_matches(|c: char| !c.is_alphabetic());
+    if !s.is_empty() && s.to_uppercase() == s {
+        "Whoa, chill out!"
     } else if phrase.trim_right().ends_with("?") {
         "Sure."
     } else {
-        let s = phrase.trim_matches(|c: char| !c.is_alphabetic());
-        if !s.is_empty() && s.to_uppercase() == s {
-            "Whoa, chill out!"
-        } else {
-            "Whatever."
-        }
+        "Whatever."
     }
 }
