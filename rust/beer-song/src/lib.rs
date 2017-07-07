a1f2df1ebe8a0028cd6cc8b18e17cfda8b3421ac
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-07-07 12:10:59 +0300

    rust/beer-song: Simplify the solution
    Use the solution by @rutaka-n:
    http://exercism.io/submissions/e39ff62ada66454da4413c483ddab1c7

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,39 +1,23 @@
 pub fn verse(n: u8) -> String {
-    format!("{} on the wall, {}.\n{}, {} on the wall.\n",
-            capitalize(amount(n)), amount(n),
-            if n == 0 {
-                "Go to the store and buy some more".to_string()
-            } else {
-                format!("Take {} down and pass it around",
-                        if n == 1 { "it" } else { "one" })
-            },
-            amount(decr(n)))
+    match n {
+        0 => format!(
+            "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => format!(
+            "1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.\n"),
+        2 => format!(
+            "2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.\n"),
+        _ => format!(
+            "{0} bottles of beer on the wall, {0} bottles of beer.
+Take one down and pass it around, {1} bottles of beer on the wall.\n", n, n-1)
+    }
 }
 
 pub fn sing(from: u8, to: u8) -> String {
-    assert!(from > to);
     (to..from+1).rev()
         .map(|n| verse(n))
         .collect::<Vec<_>>()
         .join("\n")
-}
-
-fn amount(n: u8) -> String {
-    (match n {
-        0 => String::from("no more bottles"),
-        1 => String::from("1 bottle"),
-        _ => format!("{} bottles", n),
-    }) + " of beer"
-}
-
-fn decr(n: u8) -> u8 {
-    if n == 0 { 99 } else { n-1 }
-}
-
-fn capitalize(s: String) -> String {
-    let mut s = s.chars();
-    match s.next() {
-        None => String::new(),
-        Some(c) => c.to_uppercase().chain(s).collect(),
-    }
 }
