aa7e96022716d6a790436388bbfa98b7242862d1
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-07-18 22:34:49 +0300

    rust/sum-of-multiples: Simplified the solution

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,9 +1,3 @@
 pub fn sum_of_multiples(n: u32, ds: &Vec<u32>) -> u32 {
-    let mut sum = 0;
-    for x in 1..n {
-        if ds.iter().any(|&d| x % d == 0) {
-            sum += x;
-        }
-    }
-    sum
+    (1..n).filter(|x| ds.iter().any(|d| x % d == 0)).sum()
 }
