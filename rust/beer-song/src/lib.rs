a52b13d51fabcd72fb8bbd005e48fba7d4cff5ee
Valery V. Vorotyntsev <valery.vv@gmail.com>
2017-07-21 12:19:51 +0300

    rust/beer-song: Address clippy's feedback

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,23 +1,23 @@
 pub fn verse(n: u8) -> String {
     match n {
-        0 => format!(
-            "No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.\n"),
-        1 => format!(
-            "1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.\n"),
-        2 => format!(
-            "2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.\n"),
+        0 => "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+            .to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.\n"
+            .to_string(),
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.\n"
+            .to_string(),
         _ => format!(
             "{0} bottles of beer on the wall, {0} bottles of beer.
-Take one down and pass it around, {1} bottles of beer on the wall.\n", n, n-1)
+Take one down and pass it around, {1} bottles of beer on the wall.\n", n, n-1),
     }
 }
 
 pub fn sing(from: u8, to: u8) -> String {
     (to..from+1).rev()
-        .map(|n| verse(n))
+        .map(verse)
         .collect::<Vec<_>>()
         .join("\n")
 }
