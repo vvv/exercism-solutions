ff5f265ee80fef87fe6d7b672deabd5d4e048a1c
Valery V. Vorotyntsev <valery.vv@gmail.com>
2019-05-24 00:16:38 +0300

    rust/matching-brackets: apply `cargo fmt`

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -8,38 +8,29 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack = Vec::new();
     for c in string.chars() {
         match c {
-            '[' =>
-                stack.push(Op::Square),
-            ']' =>
-                match stack.pop() {
-                    Some(Op::Square) =>
-                        continue,
-                    _ =>
-                        return false,
-                },
+            '[' => stack.push(Op::Square),
+            ']' => match stack.pop() {
+                Some(Op::Square) => continue,
+                _ => return false,
+            },
 
-            '{' =>
-                stack.push(Op::Curly),
-            '}' =>
-            {
+            '{' => stack.push(Op::Curly),
+            '}' => {
                 if let Some(Op::Curly) = stack.pop() {
                     continue;
                 }
                 return false;
             },
 
-            '(' =>
-                stack.push(Op::Round),
-            ')' =>
-            {
+            '(' => stack.push(Op::Round),
+            ')' => {
                 if let Some(Op::Round) = stack.pop() {
                     continue;
                 }
                 return false;
             },
 
-            _ =>
-                continue,
+            _ => continue,
         }
     }
     stack.is_empty()
