ae553eabc34d36ccb7fe0be828ec7a645be08779
Valery V. Vorotyntsev <valery.vv@gmail.com>
2019-05-23 20:14:47 +0300

    rust/matching-brackets solved! \o/

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,46 @@
+enum Op {
+    Square,
+    Curly,
+    Round,
+}
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    let mut stack = Vec::new();
+    for c in string.chars() {
+        match c {
+            '[' =>
+                stack.push(Op::Square),
+            ']' =>
+                match stack.pop() {
+                    Some(Op::Square) =>
+                        continue,
+                    _ =>
+                        return false,
+                },
+
+            '{' =>
+                stack.push(Op::Curly),
+            '}' =>
+            {
+                if let Some(Op::Curly) = stack.pop() {
+                    continue;
+                }
+                return false;
+            },
+
+            '(' =>
+                stack.push(Op::Round),
+            ')' =>
+            {
+                if let Some(Op::Round) = stack.pop() {
+                    continue;
+                }
+                return false;
+            },
+
+            _ =>
+                continue,
+        }
+    }
+    stack.is_empty()
 }
